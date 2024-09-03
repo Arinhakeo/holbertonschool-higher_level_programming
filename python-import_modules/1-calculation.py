80baa7511296e5b75d7bf41fd5857fa3f93da866
Arinhakeo <9295@holbertonstudents.com>
2024-09-04 00:01:56 +0200

    add

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
