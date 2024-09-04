a221dce824ae28d2d0888ffc03ccf0392bafb268
Arinhakeo <9295@holbertonstudents.com>
2024-09-04 10:51:05 +0200

    test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+    
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
