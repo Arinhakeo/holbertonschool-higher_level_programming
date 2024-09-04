d36b32955b1744e4c8692e851480d6ebccb93bb3
Arinhakeo <9295@holbertonstudents.com>
2024-09-04 10:53:27 +0200

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
-    
+
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
