b68241bb51d7b1e7a5ff1f85d763a14a9302b96d
Arinhakeo <9295@holbertonstudents.com>
2024-09-11 12:00:55 +0000

    test0

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
-    print()
-    return count
+def raise_exception_msg(message=""):
+    raise NameError(message)
