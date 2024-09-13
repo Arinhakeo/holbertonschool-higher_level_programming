a532749d8a55fd3f1f69bd7e5ba462255b8659d6
Arinhakeo <9295@holbertonstudents.com>
2024-09-13 20:15:19 +0000

    1projectreview

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
-    raise NameError(message)
+
+def safe_print_integer(value):
+    try:
+        # Attempt to print the value as an integer
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        # Handle cases where value is not an integer
+        return False
