d9f70ff6afcfbf4e5854a8e431a68947c6263f06
Arinhakeo <9295@holbertonstudents.com>
2024-09-22 08:20:39 +0000

    reviewdoc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,9 +5,5 @@ This module defines a simple Square class.
 
 class Square:
     """
-    This class defines a square.
-
-    It's an empty class that serves as a basic template for creating
-    square objects.
-    """
+    This class defines a square. """
     pass
