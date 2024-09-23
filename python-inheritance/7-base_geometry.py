5f132d1f06bfce80753a8346f1099f8aa8d18ac1
Arinhakeo <9295@holbertonstudents.com>
2024-09-23 12:05:05 +0000

    allfiles

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""Module pour l'exercice 7."""
+"""Module définissant la classe BaseGeometry."""
 
-# Insérez le code de l'exercice 7 ici
 
-if __name__ == "__main__":
-    # Code de test
-    pass
+class BaseGeometry:
+    """Classe de base pour la géométrie."""
+
+    def area(self):
+        """Calcule l'aire."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Valide que value est un entier positif."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
