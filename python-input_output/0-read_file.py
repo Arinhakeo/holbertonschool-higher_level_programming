d7dcbe5e52f74bcf3dfe6664a3d56c937cf8631a
Arinhakeo <9295@holbertonstudents.com>
2024-09-30 13:25:12 +0200

    review synthaxe ans doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module contenant une fonction pour lire un fichier."""
+
+
 def read_file(filename=""):
     """Lit un fichier et imprime son contenu."""
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
