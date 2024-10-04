a71395e12b3329e4d85e35790297c43c8b383c42
Arinhakeo <9295@holbertonstudents.com>
2024-10-04 09:45:21 +0200

    review

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,12 +3,30 @@
 
 
 import sys
-from os.path import exists
+import os
+
+# Dynamically import the required functions
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "add_item.json"
-my_list = load_from_json_file(filename) if exists(filename) else []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+def main():
+    """Main function to add command line arguments to a list and save to a
+    file."""
+    filename = "add_item.json"
+
+    # Initialise la list si elle n'existe pas
+    if os.path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
+    # Prend une ligne d'argument
+    my_list.extend(sys.argv[1:])
+
+    # Met a jour le json
+    save_to_json_file(my_list, filename)
+
+
+if __name__ == "__main__":
+    main()
