f8a873b245114265b21b21201a564839d571e055
Arinhakeo <9295@holbertonstudents.com>
2024-11-08 17:17:03 +0100

    02

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -16,5 +16,20 @@ def about():
 def contact():
     return render_template('contact.html')
 
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+        return render_template('items.html', items=data['items'])
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python 3
+
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+        return render_template('items.html', items=data['items'])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
